466e6d6869bcacbdf49d841de86e2f2e7b3e58ac
Hector Ramirez <thedraxstep1@gmail.com>
2025-02-23 23:15:05 -0500

    Recuperacion de contrasenias implementada

--- index.js
+++ index.js
@@ -1,17 +1,72 @@
 const express = require('express')
+const crypto = require('crypto')
 const bcrypt = require('bcryptjs')
+const nodemailer = require('nodemailer')
+const path = require('path')
 const pool = require('./databasepg.js')
 const cors = require('cors')
 const app = express()
 const port = process.env.port || 5000;
+const userMail = "thedraxstep1@gmail.com";
+const appPass = "viik vlrm cvnv lyuh";
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: userMail,
+        pass: appPass
+    }
+})
+
 
 app.listen(port, () => console.log('Conectado...'));
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('API Login')
+app.get('/api/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'front.html'));
 });
+
+//app.get('/api/home', (req, res) => {
+    //res.sendFile(path.join(__dirname, 'home.html'));
+//})
+
+app.post('/api/recuperacion', async (req, res) => {
+
+    let email = req.body.email;
+    let select = `SELECT * FROM users WHERE email_user = '${email}'`;
+    let result = await pool.query(select);
+
+    if(result.rows[0]){
+
+        let newPass = crypto.randomBytes(5).toString('hex');
+        let hashPass = await bcrypt.hash(newPass, 10);
+        
+        await pool.query(`UPDATE users SET password_user = '${hashPass}' WHERE email_user = '${email}'`)
+
+        let mailBody = {
+            from: userMail,
+            to: email,
+            subject: "Recuperacion de contraseña",
+            text: `Su nueva contraseña es: '${newPass}'`
+        };
+
+        transporter.sendMail(mailBody, (error, info) => {
+            if (error){
+                console.log(error);
+            }
+            console.log("Email enviado. ")
+            res.send("OK.")
+        })
+
+        
+        } else{
+            res.status(401).send("Correo no registrado.");
+        }
+
+        
+    
+
+})
 
 app.post('/api/login', async (req, res) => {
 
